5b5c61424b9ea11e1a52742141f07ba10dac9b33
alejandrosalas1 <alejandro.salas@nuocorp.com>
2022-07-09 02:15:48 -0500

    migraciones y modelo -> asalas

---
+++ app/Models/Ticket.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'ciudad_origen',
+        'ciudad_destino',
+        'ciudad_origen',
+        'fecha_salida',
+        'fecha_retorno',
+        'precio',
+        'aerolinea',
+        'numero_escalas',
+        'duracion'
+    ];
+}
---
+++ database/migrations/2022_07_09_064953_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ciudad_origen');
+            $table->string('ciudad_destino');
+            $table->string('fecha_salida');
+            $table->dateTime('fecha_retorno');
+            $table->float('precio');
+            $table->string('aerolinea');
+            $table->integer('numero_escalas');
+            $table->string('duracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
